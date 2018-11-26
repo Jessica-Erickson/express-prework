386fdb0daa8030ca162db0b70c0ae17f87bcae7d
Intrepid Panda <jerick999@gmail.com>
2018-11-26 04:45:21 -0700

    Add json response

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', (request, response) => {
   // to serve this content
 });
 
+app.get('/json', (request, response) => {
+  response.status(200).json({"name": "Robbie"});
+});
+
 app.listen(3000, () => {
   console.log('Express intro running on localhost:3000');
 });
