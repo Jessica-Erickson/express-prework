9661e371f14aaf35387fd225d18424a4d39a686f
Intrepid Panda <jerick999@gmail.com>
2018-11-26 03:36:06 -0700

    Add static files

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (request, response) => {
-  response.send('hello world');
+  // We don't need to explicitly use this handler or send a response
+  // because Express is using the default path of the static assets
+  // to serve this content
 });
 
 app.listen(3000, () => {
