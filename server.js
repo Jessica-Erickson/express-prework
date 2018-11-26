566e7e7f928253a634de914cc6cedeae4d83605e
Intrepid Panda <jerick999@gmail.com>
2018-11-26 06:33:05 -0700

    Add middleware

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 
+const urlLogger = (request, response, next) => {
+  console.log('Request URL:', request.url);
+  next();
+};
+
+const timeLogger = (request, response, next) => {
+  console.log('Datetime:', new Date(Date.now()).toString());
+  next();
+};
+
 app.use(express.static('public'));
+app.use(urlLogger, timeLogger);
 
 app.get('/', (request, response) => {
   // We don't need to explicitly use this handler or send a response
